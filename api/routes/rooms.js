abf2d07d852c1ea9e093df40e8b693f2c0486e2d
Yogesh K Gautam <yogi.2210@gmail.com>
2022-12-30 23:45:53 +0530

    finish backend server

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,22 @@
 import express from "express"
+import { createRoom, deleteRoom, getRoom, getrooms, updateRoom } from "../controllers/room.js"
+import { verifyAdmin } from "../utils/verifyToken.js"
 
 const router = express.Router()
 
+//create
+router.post("/:hotelid",verifyAdmin, createRoom)
 
+//update
+router.put("/:id",verifyAdmin, updateRoom)
+
+//delete
+router.delete("/:id/:hotelid",verifyAdmin, deleteRoom)
+
+//get
+router.get("/:id", getRoom)
+
+//get all
+router.get("/", getrooms)
 
 export default router
