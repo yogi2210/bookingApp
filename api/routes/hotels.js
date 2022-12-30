0ec30b3ccaff7aad05ead90c6b7b40a942a1e477
Yogesh K Gautam <yogi.2210@gmail.com>
2022-12-31 02:59:57 +0530

    integrate react with backend

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js"
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js"
 import Hotel from "../models/Hotel.js"
 import { createError } from "../utils/error.js"
 import { verifyAdmin } from "../utils/verifyToken.js"
@@ -16,10 +16,12 @@ router.put("/:id",verifyAdmin, updateHotel)
 router.delete("/:id",verifyAdmin, deleteHotel)
 
 //get
-router.get("/:id", getHotel)
+router.get("/find/:id", getHotel)
 
 //get all
 router.get("/", getHotels)
+router.get("/countByCity", countByCity)
+router.get("/countByType", getHotels)
 
 
 export default router
