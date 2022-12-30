cac048858c90fe59281b0462eb5eaeae1e125b0d
Yogesh K Gautam <yogi.2210@gmail.com>
2022-12-30 20:52:54 +0530

    create login and register route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,10 @@
 import express from "express"
+import { login, register } from "../controllers/auth.js"
 
 const router = express.Router()
 
-router.get("/", (req, res)=>{
-    res.send("hello this is auth end point")
-})
-router.get("/register", (req, res)=>{
-    res.send("hello this is register end point")
-})
+
+router.post("/register", register )
+router.post("/login", login )
 
 export default router
